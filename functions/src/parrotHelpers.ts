8f0a1c21b86c62e64caae65f9aa14e4fa6ef8532
Merwin Hill <merwin@q42.nl>
2022-05-06 16:40:40 +0200

    Added parrot animations

--- functions/src/parrotHelpers.ts
+++ functions/src/parrotHelpers.ts
@@ -1,13 +1,21 @@
 import { ParrotAction } from './parrotTypes';
-import { begrijpHetNietActions } from './parrotSequences';
+import { begrijpHetNietActions, overheardCommandActions } from './parrotSequences';
 
 export function GetActionByKeywords(actions: ParrotAction[], keywords: string[]): ParrotAction {
+  // Overheard action
+  if (keywords.includes('overheard') || keywords.includes('gehoord')) {
+    return GetRandomActionFromArray(overheardCommandActions);
+  }
+
+  // Search for actions that have one of the keywords
   const keywordActions = actions.filter(
     (a) => keywords.filter((k) => a.keywords.includes(k)).length > 0
   );
   if (keywordActions.length > 0) {
     return keywordActions[0];
   }
+
+  // If no actions are found, return a random 'i dont understand' action.
   return GetRandomActionFromArray(begrijpHetNietActions);
 }
 
