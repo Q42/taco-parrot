4234a8def64117672f6e367dca5e82548fc7c4a3
Merwin Hill <merwin@q42.nl>
2022-05-06 17:37:41 +0200

    Added speech recognition

--- hosting/src/components/TacoVoice.vue
+++ hosting/src/components/TacoVoice.vue
@@ -1,23 +1,111 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import type { ParrotSequence } from "../../../functions/src/parrotTypes";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Sleep } from "@/helpers";
+import { ref, onMounted } from "vue";
+import {
+  ParrotCommands,
+  type ParrotSequence,
+} from "../../../functions/src/parrotTypes";
 
 const emit = defineEmits<{
   (e: "tacoCommand", action: ParrotSequence): void;
 }>();
 
-const text = ref("");
-
-const API_URL = import.meta.env.DEV
+const QUERY_API_URL = import.meta.env.DEV
   ? "http://localhost:5001/taco-parrot/us-central1/processRequest"
   : "https://us-central1-taco-parrot.cloudfunctions.net/processRequest";
 
-async function send() {
-  // Get the sequence for the input.
+const COMMAND_API_URL = import.meta.env.DEV
+  ? "http://localhost:5001/taco-parrot/us-central1/getActionForCommand"
+  : "https://us-central1-taco-parrot.cloudfunctions.net/getActionForCommand";
+
+const text = ref("");
+const transcript = ref("");
+const isRecording = ref(false);
+const voiceControl = ref(false);
+const Recognition =
+  (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+const sr = new Recognition();
+
+let keywordDetected = false;
+
+onMounted(() => {
+  sr.continuous = true;
+  sr.interimResults = true;
+  sr.lang = "nl-NL";
+  sr.onstart = () => {
+    console.log("SR Started");
+    isRecording.value = true;
+    transcript.value = "";
+  };
+  sr.onend = () => {
+    console.log("SR Stopped");
+    isRecording.value = false;
+  };
+  sr.onresult = (evt: any) => {
+    for (let i = 0; i < evt.results.length; i++) {
+      const result = evt.results[i];
+      if (result.isFinal) CheckForCommand(result);
+    }
+    const t = Array.from(evt.results)
+      .map((result: any) => result[0])
+      .map((result) => result.transcript)
+      .join("");
+
+    transcript.value = t;
+  };
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const CheckForCommand = async (result: any) => {
+  const t = result[0].transcript as string;
+  if (t.toLowerCase().includes("taco") && !keywordDetected) {
+    sr.stop();
+    keywordDetected = true;
+    await SendCommand(ParrotCommands.WELCOME);
+    setTimeout(() => sr.start(), 100);
+  } else if (keywordDetected) {
+    keywordDetected = false;
+    sr.stop();
+    SendQuery(transcript.value.toLowerCase());
+    setTimeout(() => sr.start(), 100);
+  }
+};
+
+const ToggleMic = () => {
+  if (voiceControl.value) {
+    sr.stop();
+  } else {
+    sr.start();
+  }
+  voiceControl.value = !voiceControl.value;
+};
+
+async function SendQuery(text: string) {
   let response = (await (
-    await fetch(`${API_URL}?text=${text.value}`)
+    await fetch(`${QUERY_API_URL}?text=${text}`)
   ).json()) as ParrotSequence;
   emit("tacoCommand", response);
+  await Sleep(getSequenceTime(response));
+}
+
+async function SendCommand(command: ParrotCommands) {
+  let response = (await (
+    await fetch(`${COMMAND_API_URL}?command=${command.toString()}`)
+  ).json()) as ParrotSequence;
+  emit("tacoCommand", response);
+  await Sleep(getSequenceTime(response));
+}
+
+function getSequenceTime(sequence: ParrotSequence): number {
+  let sequenceTime = 0;
+  sequence.actions.forEach((action) => action.movements.forEach((m) => sequenceTime += m.offset));
+  return sequenceTime * 1000;
+}
+
+async function send() {
+  // Get the sequence for the input.
+  SendQuery(text.value);
 }
 </script>
 
@@ -26,8 +114,12 @@ async function send() {
     <h1 class="green">Taco emulator</h1>
     <h3>
       Talk to taco:<br />
-      <input type="text" v-model="text" />
-      <button @click="send">Send!</button>
+      <button :class="`mic`" @click="ToggleMic">
+        {{ voiceControl ? "Disable" : "Enable" }} speech recognition
+      </button>
+      <div v-if="isRecording" class="transcript" v-text="transcript"></div>
+      <input v-if="!voiceControl" type="text" v-model="text" />
+      <button v-if="!voiceControl" @click="send">Send!</button>
     </h3>
   </div>
 </template>
